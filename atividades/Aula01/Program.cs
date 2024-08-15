124786c4d422c6f854ddccd596547195da1349f3
Thiago Ernesto Thomasi <427693@unoesc.lan>
2024-08-14 22:07:41 -0300

    Meu Primeiro commit

---
+++ atividades/Aula01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Comentario de uma linha 
+/* 
+
+  Comentario de 
+  Multiplas linhas 
+
+*/ 
+
+// Declarando variaveis 
+
+int number;
+
+// Atribuindo valor 
+number = 10; 
+
+
+// Declarando constante
+const int DAYS_IN_WEEK = 7;
+
+// IMPRIMINDO STRING CONCATENADA 
+Console.WriteLine($"a semana tem {DAYS_IN_WEEK} dias.");
+
+
+
+
