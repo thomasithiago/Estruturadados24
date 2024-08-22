748d7985c60bde9dbcc3a8d5a8edf8c26a304787
Thiago Ernesto Thomasi <427693@unoesc.lan>
2024-08-21 21:51:31 -0300

    Aula Classe , Objeto Enum

--- atividades/Aula01/Program.cs
+++ atividades/Aula01/Program.cs
@@ -4,9 +4,11 @@
   Comentario de 
   Multiplas linhas 
 
-*/ 
+*/
 
 // Declarando variaveis 
+
+using Aula01;
 
 int number;
 
@@ -17,9 +19,21 @@ number = 10;
 // Declarando constante
 const int DAYS_IN_WEEK = 7;
 
-// IMPRIMINDO STRING CONCATENADA 
+// IMPRIMINDO STRING CONCATENADA
+
 Console.WriteLine($"a semana tem {DAYS_IN_WEEK} dias.");
 
+// new TipoEnumerador() -> Invoca o método construtor do objeto
+// Toda Classe tem o seu construtor padrão implicito sem argumentos 
+// Todavia,se fizermos,perdemos o original implicito e 
+// precisaremos defini-lo explicitamente 
+
+TipoEnumerador tipoEnum = new TipoEnumerador();
+
+TipoEnumerador.Language enumEnglish = 
+tipoEnum.GetLanguageEnum("inglês"); 
+
+Console.WriteLine( $"O Enum de english é {enumEnglish}" );
 
 
 
