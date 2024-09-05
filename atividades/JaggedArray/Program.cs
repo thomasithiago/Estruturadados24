2ee673869c01f0889b1db848791f18ac27213576
Thiago Ernesto Thomasi <427693@unoesc.lan>
2024-09-04 22:07:25 -0300

    Aula05

---
+++ atividades/JaggedArray/Program.cs
@@ -0,0 +1,21 @@
+﻿// JegueArray é o array do Ceará
+// JaggedArray é tambem conhecido como um vetor de Vetores 
+
+int [][] numbers = new int[4][];
+
+// Nesse caso [][] indica que é um vetor,nesse exemplo um vetor de 4 posições contendo vetores de tam indefinido 
+
+numbers[0] = new int [] { 1, 2, 3 };
+numbers[1] = new int [] { 4,3,2,1,0};
+numbers[2] = new int [] { 1,2,0};
+numbers[3] = new int [] { 57 } ;
+numbers[4] = null; 
+
+// Para obter valor de jaggedArray 
+int valor = numbers[0][2];
+Console.WriteLine(valor);
+
+// Para atribuir valor em um jaggedArray 
+
+numbers[1][2] =0;
+Console.WriteLine( numbers [1][2] );
---
+++ atividades/ListaSimples/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+//Utilizando a Lista Simples 
+ArrayList arrList = new ();
+arrList.Add(5);
+arrList.Add("Thiago");
+// É possivel aidiconar um conjunto de valores de uma só vez 
+arrList.AddRange(new int [] { 1, 2, 3});
+// o método .Add() insere o valor ao fial do vetor 
+arrList.Insert(2, 15);
+// Já o metodo inseert me permite incluior o valor desejado na posição especificada no primeiro parâmetro;
+
+//Lendo valores da Lista 
+object primeiro = arrList[0]!;
+int quarto = (int)arrList[3]!;
+
+// Percorrendo a lista com foreach 
+foreach(object obj in arrList)
+{
+  
+   Console.Write($" | {obj}");
+
+}
+
+//obtendo o tamanho total da lista 
+
+int tamanho = arrList.Count;
+// Obter a capacidade 
+// Quandos podem ser armazenados 
+int capacidade = arrList.Capacity;
+
+// Percorrendo no modo Clássico 
+for(int i =0; i < arrList.Count; i++)
+{
+    Console.Write( $" | {arrList[i]}" );
+}
+
+// Recursos importantes da Lista 
+// Veirificar se um valor é contido na lista 
+
+bool contemNome = arrList.Contains("Thiago");
+if(!contemNome )
+  Console.WriteLine("Nome Thiago não encontrado");
+
+  
+
+   // Para saber se o índice que contem o valor buscado 
+   // Neste caso se ovalor existir na lista 
+   // Ela reotorna o índice (número inteiro)
+   // Caso o valor não exista na lista, retorna -1
+   int indiceValor =
+     arrList.IndexOf("Thiago");
+     if(indiceValor >= 0)
+        Console.WriteLine( $"Thiago está em [{indiceValor}]");
+        else 
+
+        Console.WriteLine("É, realmente não tem");
+
+        //É possivel remover itens da lista 
+        arrList.Remove("Thiago");
+        arrList.RemoveAt(4);
+        arrList.RemoveRange(0,2);
