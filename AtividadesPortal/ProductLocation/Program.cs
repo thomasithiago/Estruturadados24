df287159e06d366cc9de25b59daf0586423c96e9
Thiago Ernesto Thomasi <427693@unoesc.lan>
2024-09-25 22:01:30 -0300

    LocalizaçãodeProduto

---
+++ AtividadesPortal/ProductLocation/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+
+namespace ProductLocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creating and initializing a dictionary
+            Dictionary<string, string> productLocations = new Dictionary<string,string>
+            {
+                // Adding key-value pairs to the dictionary
+                {"89", "A1"},
+                {"90", "A2"},
+                {"91", "A3"},
+                {"92", "A4"},
+                {"93", "A5"},
+                {"94", "A6"},
+                {"95", "A7"},
+                {"96", "A8"},
+                {"97", "A9"}
+
+            };
+
+            // Adding a new key-value pair to the dictionary
+            productLocations["101"] = "B1";
+            productLocations["102"] = "B2";
+            productLocations["103"] = "B3";
+            productLocations["104"] = "B4";
+            productLocations["105"] = "B5";
+            productLocations["106"] = "B6";
+            productLocations["107"] = "B7";
+            productLocations["108"] = "B8";
+            productLocations["109"] = "B9";
+
+            try
+            {
+                // Adding a key-value pair to the dictionary with a different method 
+                productLocations.Add("205", "C1");
+                productLocations.Add("206","C2");
+                productLocations.Add("207","C3");
+                productLocations.Add("208","C4");
+                productLocations.Add("209","C5");
+                productLocations.Add("210","C6");
+                productLocations.Add("211","C7");
+                productLocations.Add("212","C8");
+                productLocations.Add("213","C9");
+            }
+            catch (ArgumentException)
+            {
+                // If the key already exists, catch the exception and display a message
+                Console.WriteLine("Product already exists!");
+            }
+
+            Console.WriteLine("All Products: ");
+
+            // If there are no products, display a message
+            if(productLocations.Count == 0)
+            {
+                Console.WriteLine("No products found!");
+            }
+
+            else
+            {
+                // Iterate through the dictionary and display the key-value pairs
+                foreach (KeyValuePair<string, string> product in productLocations)
+                {
+                    Console.WriteLine($"Product ID: {product.Key}, Location: {product.Value}");
+                }
+            }
+
+            Console.WriteLine();
+
+            // Search manually for a product with the ID
+            Console.WriteLine("Search by Product ID: ");
+            string productId = Console.ReadLine();
+
+            if(productLocations.TryGetValue(productId, out string location))
+            {
+                // If the product is found, display the location
+                Console.WriteLine($"The product is located at {location}.");
+            }
+
+            else
+            {
+                // If the product is not found, display a message
+                Console.WriteLine("Product not found!");
+            }
+        }
+    }
+}
---
+++ Conteudos/Dicionario/Program.cs
@@ -0,0 +1,58 @@
+﻿// Diferente da TabelaHash, O Dictionary nos permite expecificar o tipo de dado que sera utulizado nos PARES 
+// Key --. Value 
+//Isso nos fornece uma estrutura fortemente tipada 
+
+Dictionary<string, string> dictionary = new Dictionary<string, string>();
+{
+   {"Chave 1" , "Valor 1"}
+   {"Chave 2" , "Valor 2"}
+
+};
+
+//Obter o valor do dictionary 
+string val = dictionary["Chave 1"];
+
+try
+{
+    val = dictionary["Chave 3"];
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Eroo ao obter valor da Chave 3");
+    Console.WriteLine(ex.Message);
+}
+
+if(dictionary.ContainsKey("Chave 2"))
+
+{
+
+    Console.WriteLine($"Chave 2: {dictionary["Chave 2"]}");
+
+}
+
+//Outra forma de obter um valor e evitar erro
+
+var valor = dictionary.TryGetValue("Chave 4" , out string? valor);
+if val(valor is not null)
+     Console.WriteLine($"Chave 4: {dictionary["Chave 4"]}");
+
+//Podemos adcionar da mesma foram pela chave inexistente 
+dictionary ["Chave 0"] = "Valor 0";
+
+foreach(KeyValuePair<string, string> kvp in dictionary)
+{
+  Console.WriteLine($"{kvp.Key}:{kvp.Value}");   
+}
+
+dictionary<int , Pessoa> dicPessoa =
+ new dictionary<int, Pessoa>();
+
+ dicPessoa.Add(
+    1,
+    new Pessoa () {
+        id = 0,
+        BirthDate = new DateTime (2005 ,6 ,6 ),
+        nameof = "Thiago Thomasi"
+    }
+ );
